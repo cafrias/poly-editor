ee9856884a027ebaa42ccc75f1f674e46cdb6e05
Carlos Frias <carlos.a.frias@gmail.com>
2018-05-09 14:05:51 -0300

    Add delete polygon

--- src/index.js
+++ src/index.js
@@ -64,6 +64,15 @@ PolyEditor.prototype.AddEventsToPolygon = function (poly) {
   google.maps.event.addListener(poly.getPath(), 'set_at', this.updateField.bind(this));
   google.maps.event.addListener(poly.getPath(), 'insert_at', this.updateField.bind(this));
   google.maps.event.addListener(poly.getPath(), 'remove_at', this.updateField.bind(this));
+
+  google.maps.event.addListener(poly, 'rightclick', (function (poly, e) {
+    var del = new this.DeleteMenu(this.deletePolygon.bind(this));
+    del.open(this.map, this.poly, e.latLng);
+  }).bind(this, poly));
+};
+
+PolyEditor.prototype.deletePolygon = function (poly) {
+  console.log('delete poly');
 };
 
 /**
@@ -133,6 +142,7 @@ PolyEditor.polyOptions = {
 PolyEditor.prototype.init = function () {
   this.initMap();
   this.initDrawManager();
+  this.initDeleteMenu();
 };
 
 /**
@@ -166,4 +176,95 @@ PolyEditor.prototype.initDrawManager = function () {
   google.maps.event.addListener(this.drawManager, 'polygoncomplete', this.setPolygon.bind(this));
 
   this.drawManager.setMap(this.map);
+};
+
+
+// POLYGON DELETE MENU ________________________________________________________________________________________________
+
+/**
+ * Wraps DeleteMenu so that it can be executed after Google Maps has been initialized.
+ */
+PolyEditor.prototype.initDeleteMenu = function () {
+  /**
+   * A menu that lets a user delete a polygon
+   * @constructor
+   */
+  this.DeleteMenu = function (delCb) {
+    // Set CSS for the control interior
+    var controlText = document.createElement('div');
+    controlText.style.backgroundColor = '#fff';
+    controlText.style.border = '2px solid #fff';
+    controlText.style.borderRadius = '3px';
+    controlText.style.zIndex = '10';
+    controlText.style.boxShadow = '0 2px 6px rgba(0,0,0,.3)';
+    controlText.style.cursor = 'pointer';
+    controlText.style.marginBottom = '22px';
+    controlText.style.position = 'absolute';
+    controlText.style.color = 'rgb(25,25,25)';
+    controlText.style.fontFamily = 'Roboto,Arial,sans-serif';
+    controlText.style.fontSize = '16px';
+    controlText.style.lineHeight = '38px';
+    controlText.style.paddingLeft = '5px';
+    controlText.style.paddingRight = '5px';
+    controlText.innerHTML = 'Eliminar';
+
+    this.div_ = controlText;
+
+    google.maps.event.addDomListener(controlText, 'click', (function () {
+      delCb();
+      this.close();
+    }).bind(this));
+  };
+
+  this.DeleteMenu.prototype = new google.maps.OverlayView();
+
+  this.DeleteMenu.prototype.onAdd = function () {
+    var deleteMenu = this;
+    var map = this.getMap();
+    this.getPanes().floatPane.appendChild(this.div_);
+
+    // mousedown anywhere on the map except on the menu div will close the
+    // menu.
+    this.divListener_ = google.maps.event.addDomListener(map.getDiv(), 'mousedown', function (e) {
+      if (e.target != deleteMenu.div_) {
+        deleteMenu.close();
+      }
+    }, true);
+  };
+
+  this.DeleteMenu.prototype.onRemove = function () {
+    google.maps.event.removeListener(this.divListener_);
+    this.div_.parentNode.removeChild(this.div_);
+
+    // clean up
+    this.set('position');
+    this.set('polygon');
+  };
+
+  this.DeleteMenu.prototype.close = function () {
+    this.setMap(null);
+  };
+
+  this.DeleteMenu.prototype.draw = function () {
+    var position = this.get('position');
+    var projection = this.getProjection();
+
+    if (!position || !projection) {
+      return;
+    }
+
+    var point = projection.fromLatLngToDivPixel(position);
+    this.div_.style.top = point.y + 'px';
+    this.div_.style.left = point.x + 'px';
+  };
+
+  /**
+   * Opens the menu at a vertex of a given path.
+   */
+  this.DeleteMenu.prototype.open = function (map, poly, position) {
+    this.set('polygon', poly);
+    this.set('position', position);
+    this.setMap(map);
+    this.draw();
+  };
 };
